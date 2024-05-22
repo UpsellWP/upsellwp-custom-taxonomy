570d273a417153aba03ea818e13207671582bc37
AnanthFLycart <anantharaj@flycart.org>
2024-05-22 18:28:44 +0530

    - Improve Updater

--- src/UWP_GH_Updater.php
+++ src/UWP_GH_Updater.php
@@ -12,7 +12,7 @@ class UWP_GH_Updater
      *
      * @var mixed
      */
-    private $file, $plugin, $basename, $active, $username, $repository, $authorize_token, $github_response;
+    private $file, $plugin, $basename, $username, $repository, $authorize_token, $github_response;
 
     /**
      * Constructor.
@@ -47,31 +47,30 @@ class UWP_GH_Updater
         add_filter('plugins_api', array($this, 'pluginPopup'), 10, 3);
         add_filter('upgrader_post_install', array($this, 'afterInstall'), 10, 3);
 
-        // Add Authorization Token to downloadPackage
-        add_filter('upgrader_pre_download', function () {
-            add_filter('http_request_args', [$this, 'downloadPackage'], 15, 2);
-            return false; // upgrader_pre_download filter default return value.
-        });
+        if (!empty($this->authorize_token)) {
+            add_filter('upgrader_pre_download', function ($status) {
+                add_filter('http_request_args', [$this, 'addTokenInHeader'], 15, 2);
+                return $status; // upgrader_pre_download filter default return value
+            });
+        }
     }
 
     /**
-     * Get repo info.
-     *
-     * @return void
+     * Fetch latest release.
      */
-    private function getRepositoryInfo()
+    private function fetchLatestRelease()
     {
         if (is_null($this->github_response)) { // Do we have a response?
             $args = array();
-            $request_uri = sprintf('https://api.github.com/repos/%s/%s/releases', $this->username, $this->repository); // Build URI
+            $request_uri = sprintf('https://api.github.com/repos/%s/%s/releases/latest', $this->username, $this->repository); // Build URI
             if ($this->authorize_token) { // Is there an access token?
                 $args['headers']['Authorization'] = 'token {$this->authorize_token}'; // Set the headers
             }
-            $response = json_decode(wp_remote_retrieve_body(wp_remote_get($request_uri, $args)), true); // Get JSON and parse it
-            if (is_array($response)) { // If it is an array
-                $response = current($response); // Get the first item
+            $response = wp_remote_get($request_uri, $args);
+            if (!is_wp_error($response) && wp_remote_retrieve_response_code($response) == 200) {
+                $response = json_decode(wp_remote_retrieve_body($response), true);
             }
-            $this->github_response = $response; // Set it to our property
+            $this->github_response = is_array($response) && isset($response['tag_name']) ? $response : array();
         }
     }
 
@@ -84,7 +83,6 @@ class UWP_GH_Updater
     {
         $this->plugin = get_plugin_data($this->file);
         $this->basename = plugin_basename($this->file);
-        $this->active = function_exists('is_plugin_active') && is_plugin_active($this->basename);
     }
 
     /**
@@ -96,21 +94,23 @@ class UWP_GH_Updater
     public function modifyTransient($transient)
     {
         if (is_object($transient) && property_exists($transient, 'checked')) { // Check if transient has a checked property
-            if ($transient->checked && $transient->checked[$this->basename]) { // Did WordPress check for updates?
-                $this->getRepositoryInfo(); // Get the repo info
-                $github_version = ltrim($this->github_response['tag_name'], 'v');
-                $local_version = $transient->checked[$this->basename];
-                $out_of_date = version_compare($github_version, $local_version, 'gt'); // Check if we're out of date
-                if ($out_of_date) {
-                    $new_files = $this->github_response['zipball_url']; // Get the ZIP
-                    $slug = current(explode('/', $this->basename)); // Create valid slug
-                    $plugin = array( // setup our plugin info
-                        'url' => $this->plugin['PluginURI'],
-                        'slug' => $slug,
-                        'package' => $new_files,
-                        'new_version' => ltrim($this->github_response['tag_name'], 'v'),
-                    );
-                    $transient->response[$this->basename] = (object)$plugin; // Return it in response
+            if ($transient->checked && isset($transient->checked[$this->basename])) { // Did WordPress check for updates?
+                $this->fetchLatestRelease(); // Get the repo info
+                if (!empty($this->github_response)) {
+                    $github_version = ltrim($this->github_response['tag_name'], 'v');
+                    $local_version = $transient->checked[$this->basename];
+                    $out_of_date = version_compare($github_version, $local_version, 'gt'); // Check if we're out of date
+                    if ($out_of_date) {
+                        $new_files = $this->github_response['zipball_url']; // Get the ZIP
+                        $slug = current(explode('/', $this->basename)); // Create valid slug
+                        $plugin = array( // setup our plugin info
+                            'url' => $this->plugin['PluginURI'],
+                            'slug' => $slug,
+                            'package' => $new_files,
+                            'new_version' => ltrim($this->github_response['tag_name'], 'v'),
+                        );
+                        $transient->response[$this->basename] = (object)$plugin; // Return it in response
+                    }
                 }
             }
         }
@@ -124,31 +124,32 @@ class UWP_GH_Updater
     {
         if (!empty($args->slug)) { // If there is a slug
             if ($args->slug == current(explode('/', $this->basename))) { // And it's our slug
-                $this->getRepositoryInfo(); // Get our repo info
-
-                // Set it to an array
-                $plugin = array(
-                    'name' => $this->plugin['Name'],
-                    'slug' => $this->basename,
-                    'requires' => '5.3.0',
-                    // 'tested' => '',
-                    // 'rating' => '',
-                    // 'ratings' => '',
-                    // 'downloaded' => '',
-                    'added' => '2024-05-22',
-                    'version' => ltrim($this->github_response['tag_name'], 'v'),
-                    'author' => $this->plugin['AuthorName'],
-                    'author_profile' => $this->plugin['AuthorURI'],
-                    'last_updated' => $this->github_response['published_at'],
-                    'homepage' => $this->plugin['PluginURI'],
-                    'short_description' => $this->plugin['Description'],
-                    'sections' => array(
-                        'Description' => $this->plugin['Description'],
-                        'Updates' => $this->github_response['body'],
-                    ),
-                    'download_link' => $this->github_response['zipball_url'],
-                );
-                return (object)$plugin; // Return the data
+                $this->fetchLatestRelease(); // Get our repo info
+                if (!empty($this->github_response)) {
+                    // Set it to an array
+                    $plugin = array(
+                        'name' => $this->plugin['Name'],
+                        'slug' => $this->basename,
+                        'requires' => '5.3.0',
+                        // 'tested' => '',
+                        // 'rating' => '',
+                        // 'ratings' => '',
+                        // 'downloaded' => '',
+                        'added' => '2024-05-22',
+                        'version' => ltrim($this->github_response['tag_name'], 'v'),
+                        'author' => $this->plugin['AuthorName'],
+                        'author_profile' => $this->plugin['AuthorURI'],
+                        'last_updated' => $this->github_response['published_at'],
+                        'homepage' => $this->plugin['PluginURI'],
+                        'short_description' => $this->plugin['Description'],
+                        'sections' => array(
+                            'Description' => $this->plugin['Description'],
+                            'Updates' => $this->github_response['body'],
+                        ),
+                        'download_link' => $this->github_response['zipball_url'],
+                    );
+                    return (object)$plugin; // Return the data
+                }
             }
         }
         return $result; // Otherwise return default
@@ -157,14 +158,14 @@ class UWP_GH_Updater
     /**
      * Download package.
      */
-    public function downloadPackage($args, $url)
+    public function addTokenInHeader($args, $url)
     {
         if (null !== $args['filename']) {
             if ($this->authorize_token) {
-                $args = array_merge($args, array('headers' => array('Authorization' => 'token {$this->authorize_token}')));
+                $args = array_merge($args, array('headers' => array('Authorization' => "token {$this->authorize_token}")));
             }
         }
-        remove_filter('http_request_args', [$this, 'downloadPackage']);
+        remove_filter('http_request_args', [$this, 'addTokenInHeader']);
         return $args;
     }
 
@@ -173,13 +174,14 @@ class UWP_GH_Updater
      */
     public function afterInstall($response, $hook_extra, $result)
     {
-        global $wp_filesystem; // Get global FS object
-        $install_directory = plugin_dir_path($this->file); // Our plugin directory
-        $wp_filesystem->move($result['destination'], $install_directory); // Move files to the plugin dir
-        $result['destination'] = $install_directory; // Set the destination for the rest of the stack
-
-        if ($this->active) { // If it was active
-            activate_plugin($this->basename); // Reactivate
+        if (!empty($result['destination']) && strpos($result['destination'], dirname($this->basename)) !== false) {
+            global $wp_filesystem; // Get global FS object
+            $install_directory = plugin_dir_path($this->file); // Our plugin directory
+            $wp_filesystem->move($result['destination'], $install_directory); // Move files to the plugin dir
+            $result['destination'] = $install_directory; // Set the destination for the rest of the stack
+            if (!empty($result['destination_name'])) {
+                $result['destination_name'] = dirname($this->basename);
+            }
         }
         return $result;
     }
